1b17ece910edd20660b10d571345b1c8805fb88d
Francescodc92 <francescodicorpo1992@hotmail.com>
2025-07-25 16:14:07 +0200

    create loader component

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,9 +1,16 @@
 import { FooterComponent } from "@/components/footer/footer-component";
 import { GoUpButtonComponent } from "@/components/go-up-button/go-up-button";
 import { HeaderComponent } from "@/components/header/header-component";
-import type { ReactNode } from "react";
+import { useEffect, type ReactNode } from "react";
+import { useLocation } from "react-router-dom";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({ top: 0, behavior: "smooth" })
+    }, [pathname]);
+
     return (
         <>
             <HeaderComponent />
